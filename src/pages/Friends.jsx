986b57e97b1739adecd46fd3646dfcc98fad5064
SianCs <sian.mithuna@hotmail.co.th>
2025-06-26 15:00:13 +0700

    start_ Login_page

---
+++ src/pages/Friends.jsx
@@ -0,0 +1,6 @@
+function Friends() {
+  return (
+    <div>Friends Page</div>
+  )
+}
+export default Friends
---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+function Home() {
+  return (
+    <div>User Home</div>
+  )
+}
+export default Home
